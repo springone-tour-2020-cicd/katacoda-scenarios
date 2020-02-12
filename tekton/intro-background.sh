c1008c2dd945ae6e3ee1077c65b63f7f59c91a77
Mark Pollack <mpollack@pivotal.io>
2020-02-12 13:43:28 -0500

    add background downloading of scenario resources

---
+++ tekton/intro-background.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/springone-tour-2020-cicd/tekton-labs.git
+curl -LO https://github.com/tektoncd/cli/releases/download/v0.7.1/tkn_0.7.1_Linux_x86_64.tar.gz
+sudo tar xvzf tkn_0.7.1_Linux_x86_64.tar.gz -C /usr/local/bin/ tkn
