e8fc4f58e8beaae59cc8586a673a6dc34885ca1a
Mark Pollack <mpollack@pivotal.io>
2020-02-12 13:53:00 -0500

    Add process spinner and start of minikube

--- tekton/intro-background.sh
+++ tekton/intro-background.sh
@@ -1,3 +1,6 @@
 git clone https://github.com/springone-tour-2020-cicd/tekton-labs.git
 curl -LO https://github.com/tektoncd/cli/releases/download/v0.7.1/tkn_0.7.1_Linux_x86_64.tar.gz
 sudo tar xvzf tkn_0.7.1_Linux_x86_64.tar.gz -C /usr/local/bin/ tkn
+echo "done" >> /root/tool-install-finished
+minikube start
+echo "done" >> /root/minikube-start-finished
