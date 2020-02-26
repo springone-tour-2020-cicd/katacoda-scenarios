8043fe4047b9c429a7c4daf16eacf9e3eb84320c
Cora Iberkleid <ciberkleid@gmail.com>
2020-02-26 17:54:33 -0500

    fix background script

--- argo/intro-background.sh
+++ argo/intro-background.sh
@@ -18,3 +18,4 @@ echo "### Finished configuring git global settings"
 
 ########## NO CHANGES BELOW THIS LINE ##########
 # The following must be the last line
+echo "done" >> /root/environment-prepared
