6dd9e81427faba7ee89e9952e7cb791607b998f2
Cora Iberkleid <ciberkleid@gmail.com>
2020-06-09 07:22:22 -0400

    add newline

--- argocd/intro-foreground.sh
+++ argocd/intro-foreground.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export PS1="\[\033[0m\]\w\$ " JAVA_HOME=/usr/lib/jvm/default-java/jre MY_GITHUB_ORG=springone-tour-2020-cicd
+export PS1="\n\[\033[0m\]\w\$ " JAVA_HOME=/usr/lib/jvm/default-java/jre MY_GITHUB_ORG=springone-tour-2020-cicd
 until `which init-env`; do sleep 1; done;
