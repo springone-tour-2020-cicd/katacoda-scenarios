ea7af4d42b1159a1e5c39b805ee21bf35a19d03d
Cora Iberkleid <ciberkleid@gmail.com>
2020-06-14 11:09:39 -0400

    testing changes

--- 1-intro-workflow/intro-foreground.sh
+++ 1-intro-workflow/intro-foreground.sh
@@ -1,3 +1,7 @@
-#!/bin/bash
+#!/bin/bash -ex
+{ set +x; } 2>/dev/null
 
 until `which init-controller`; do sleep 1; done;
+until `which init-foreground`; do sleep 1; done;
+
+echo "Environment ready!"
