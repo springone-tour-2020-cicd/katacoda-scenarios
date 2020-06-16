fdf4129f266aeb1312372ba22d0caf81ee48cd96
Cora Iberkleid <ciberkleid@gmail.com>
2020-06-16 10:17:23 -0400

    add source init-foreground

--- 1-intro-workflow-with-buildpacks/intro-foreground.sh
+++ 1-intro-workflow-with-buildpacks/intro-foreground.sh
@@ -1,7 +1,2 @@
 #!/bin/bash +x
-
-PATH=/root/init-env/bin:$PATH
-until `which init-controller`; do sleep 1; done;
-source init-foreground
-
-echo "Environment ready!"
+PATH=/root/init-env/bin:$PATH; until `which init-controller`; do sleep 1; done; source init-foreground; echo "Environment ready!"
